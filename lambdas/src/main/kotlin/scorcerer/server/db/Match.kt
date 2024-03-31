56b4be8581c1b6195234e79c0e26085582ae9557
Luke Ely <elyluk@amazon.com>
2024-03-31 22:40:32 +0100

    Remove uneeded files

--- lambdas/src/main/kotlin/scorcerer/server/db/Match.kt
+++
@@ -1,9 +0,0 @@
-package scorcerer.server.db
-
-import org.jetbrains.exposed.dao.id.IntIdTable
-import org.jetbrains.exposed.sql.Column
-
-object Match: IntIdTable() {
-    val homeTeamName: Column<String> = varchar("home_team_name", 50)
-    val awayTeamName: Column<String> = varchar("away_team_name", 50)
-}
