d425132cfab36e8894999aa7a45bdac232cbbb29
Simon Archer <simon.james.archer@gmail.com>
2024-04-03 07:52:44 +0100

    refactor: tidy up function with 'when':

--- lambdas/src/main/kotlin/scorcerer/utils/PointsCalculator.kt
+++ lambdas/src/main/kotlin/scorcerer/utils/PointsCalculator.kt
@@ -8,35 +8,11 @@ class PointsCalculator {
     fun calculatePoints(
         prediction: Prediction,
         result: Result,
-    ): Int {
-        // correct score 5 points
-        if (
-            prediction.awayScore == result.awayScore &&
-            prediction.homeScore == result.homeScore
-        ) {
-            return 5
-        }
-
-        // correct result 2 points
-        if (
-            prediction.homeScore < prediction.awayScore &&
-            result.homeScore < result.awayScore
-        ) {
-            return 2
-        }
-        if (
-            prediction.homeScore > prediction.awayScore &&
-            result.homeScore > result.awayScore
-        ) {
-            return 2
-        }
-        if (
-            prediction.homeScore == prediction.awayScore &&
-            result.homeScore == result.awayScore
-        ) {
-            return 2
-        }
-
-        return 0
+    ) = when {
+        prediction.awayScore == result.awayScore && prediction.homeScore == result.homeScore -> 5
+        prediction.homeScore < prediction.awayScore && result.homeScore < result.awayScore -> 2
+        prediction.homeScore > prediction.awayScore && result.homeScore > result.awayScore -> 2
+        prediction.homeScore == prediction.awayScore && result.homeScore == result.awayScore -> 2
+        else -> 0
     }
 }
