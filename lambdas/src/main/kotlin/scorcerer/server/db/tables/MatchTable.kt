6cc95a583a0a43e9c181d88dbbe18a2e32ebe9e3
Simon Archer <simon.james.archer@gmail.com>
2024-04-27 20:01:49 +0100

    Filling in some more tables

--- lambdas/src/main/kotlin/scorcerer/server/db/tables/MatchTable.kt
+++ lambdas/src/main/kotlin/scorcerer/server/db/tables/MatchTable.kt
@@ -1,19 +1,32 @@
 package scorcerer.server.db.tables
 
-import org.ktorm.schema.Table
-import org.ktorm.schema.int
-import org.ktorm.schema.varchar
+import org.ktorm.schema.*
 
 /*
 create table match(
     id serial primary key,
-    home_team varchar(30) not null,
-    away_team varchar(30) not null
+    home_team_id varchar(30) not null,
+    away_team_id varchar(30) not null,
+    datetime datetime() not null,
+    home_score int() not null,
+    away_score int() not null,
+    result varchar(10),
+    state varchar(10),
+    venue varchar(20) not null,
+    matchDay int() not null
 );
  */
 
 object MatchTable : Table<Nothing>("match") {
     val id = int("id").primaryKey()
-    val homeTeam = varchar("home_team")
-    val awayTeam = varchar("away_team")
+    val homeTeamId = varchar("home_team_id")
+    val awayTeamId = varchar("away_team_id")
+    val datetime = datetime("datetime")
+    val homeScore = int("home_score")
+    val awayScore = int("away_socre")
+    val result = varchar("result")
+    val state = varchar("state")
+    val venue = varchar("venue")
+    val matchDay = int("match_day")
+
 }
---
+++ lambdas/src/main/kotlin/scorcerer/server/db/tables/TeamTable.kt
@@ -0,0 +1,19 @@
+package scorcerer.server.db.tables
+
+import org.ktorm.schema.Table
+import org.ktorm.schema.int
+import org.ktorm.schema.varchar
+
+/*
+create table team(
+    id serial primary key,
+    name varchar(30) not null,
+    flag_uri varchar(60) not null,
+);
+ */
+
+object TeamTable : Table<Nothing>("prediction") {
+    val id = int("id").primaryKey()
+    val name = varchar("name")
+    val flagUri = int("flag_uri")
+}
