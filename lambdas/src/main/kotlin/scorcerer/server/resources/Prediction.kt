0b74cc20d327255f1aa166a9323e6fe9246c79e3
Simon Archer <simon.james.archer@gmail.com>
2024-05-16 20:41:44 +0100

    allow-update-prediction

--- lambdas/src/main/kotlin/scorcerer/server/resources/Prediction.kt
+++ lambdas/src/main/kotlin/scorcerer/server/resources/Prediction.kt
@@ -1,18 +1,43 @@
 package scorcerer.server.resources
 
+import org.jetbrains.exposed.sql.and
 import org.jetbrains.exposed.sql.insert
+import org.jetbrains.exposed.sql.selectAll
 import org.jetbrains.exposed.sql.transactions.transaction
+import org.jetbrains.exposed.sql.update
 import org.openapitools.server.apis.PredictionApi
 import org.openapitools.server.models.CreatePrediction200Response
 import org.openapitools.server.models.CreatePredictionRequest
-import org.postgresql.util.PSQLException
+import scorcerer.server.db.tables.MatchResult
 import scorcerer.server.db.tables.PredictionTable
-import scorcerer.utils.throwDatabaseError
 
 class Prediction : PredictionApi() {
-    override fun createPrediction(requesterUserId: String, createPredictionRequest: CreatePredictionRequest): CreatePrediction200Response {
-        val id = try {
+    override fun createPrediction(
+        requesterUserId: String,
+        createPredictionRequest: CreatePredictionRequest,
+    ): CreatePrediction200Response {
+        var predictionId = transaction {
+            PredictionTable.selectAll()
+                .where { (PredictionTable.memberId eq requesterUserId).and(PredictionTable.matchId eq createPredictionRequest.matchId.toInt()) }
+                .firstOrNull()?.let { row ->
+                    row[PredictionTable.id]
+                }
+        }
+
+        predictionId?.let { id ->
             transaction {
+                PredictionTable.update({ PredictionTable.id eq id }) {
+                    it[homeScore] = createPredictionRequest.homeScore
+                    it[awayScore] = createPredictionRequest.awayScore
+                    it[result] = if (createPredictionRequest.toGoThrough != null) {
+                        MatchResult.valueOf(createPredictionRequest.toGoThrough.value)
+                    } else {
+                        null
+                    }
+                }
+            }
+        } ?: run {
+            predictionId = transaction {
                 PredictionTable.insert {
                     it[this.memberId] = requesterUserId
                     it[this.matchId] = createPredictionRequest.matchId.toInt()
@@ -20,10 +45,8 @@ class Prediction : PredictionApi() {
                     it[this.awayScore] = createPredictionRequest.awayScore
                 } get PredictionTable.id
             }
-        } catch (e: PSQLException) {
-            throwDatabaseError(e, "Prediction already exists, use updatePrediction instead")
         }
 
-        return CreatePrediction200Response(id.toString())
+        return CreatePrediction200Response(predictionId.toString())
     }
 }
