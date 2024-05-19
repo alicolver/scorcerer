873bbb7b5a346e291d99589da18a13ed7e7118ae
Simon Archer <simon.james.archer@gmail.com>
2024-05-19 09:42:10 +0100

    make-member-id-40

--- lambdas/src/main/kotlin/scorcerer/server/db/tables/LeagueMembershipTable.kt
+++ lambdas/src/main/kotlin/scorcerer/server/db/tables/LeagueMembershipTable.kt
@@ -4,6 +4,6 @@ import org.jetbrains.exposed.sql.Table
 
 object LeagueMembershipTable : Table("league_membership") {
     val id = integer("id").uniqueIndex().autoIncrement()
-    val memberId = varchar("member_id", 30).references(MemberTable.id)
+    val memberId = varchar("member_id", 40).references(MemberTable.id)
     val leagueId = varchar("league_id", 30).references(LeagueTable.id)
 }
--- lambdas/src/main/kotlin/scorcerer/server/db/tables/PredictionTable.kt
+++ lambdas/src/main/kotlin/scorcerer/server/db/tables/PredictionTable.kt
@@ -5,7 +5,7 @@ import scorcerer.server.db.tables.LeagueMembershipTable.references
 
 object PredictionTable : Table("prediction") {
     val id = integer("id").uniqueIndex().autoIncrement()
-    val memberId = varchar("member_id", 30).references(MemberTable.id)
+    val memberId = varchar("member_id", 40).references(MemberTable.id)
     val matchId = integer("match_id").references(MatchTable.id)
     val homeScore = integer("home_score").check { it.greaterEq(0) }
     val awayScore = integer("away_score").check { it.greaterEq(0) }
