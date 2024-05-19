c42d7a2eb27cc5204f8b9d1c9bd58f72c157bfdb
Luke Ely <elyluk@amazon.com>
2024-05-19 17:54:34 +0100

    Comment out broken code

--- lambdas/src/main/kotlin/scorcerer/server/events/UserCreationEventHandler.kt
+++ lambdas/src/main/kotlin/scorcerer/server/events/UserCreationEventHandler.kt
@@ -1,22 +1,14 @@
 package scorcerer.server.events
 
-import aws.sdk.kotlin.services.s3.S3Client
 import com.amazonaws.services.lambda.runtime.Context
 import com.amazonaws.services.lambda.runtime.RequestHandler
 import com.amazonaws.services.lambda.runtime.events.SQSEvent
-import kotlinx.coroutines.runBlocking
 import org.jetbrains.exposed.sql.insert
 import org.jetbrains.exposed.sql.transactions.transaction
 import org.openapitools.server.kotshiJson
-import org.postgresql.util.PSQLException
-import scorcerer.server.Environment
 import scorcerer.server.db.Database
-import scorcerer.server.db.tables.LeagueMembershipTable
-import scorcerer.server.db.tables.LeagueTable
 import scorcerer.server.db.tables.MemberTable
 import scorcerer.server.log
-import scorcerer.utils.LeaderboardS3Service
-import scorcerer.utils.caclulateGlobalLeaderboard
 import se.ansman.kotshi.JsonSerializable
 
 @JsonSerializable
@@ -46,30 +38,30 @@ class UserCreationEventHandler : RequestHandler<SQSEvent, Unit> {
                 }
             }
 
-            try {
-                transaction {
-                    LeagueTable.insert {
-                        it[this.name] = "Global"
-                        it[this.id] = "global"
-                    }
-                }
-            } catch (e: PSQLException) {
-                log.info("Global league exists: $e")
-            }
-
-            transaction {
-                LeagueMembershipTable.insert {
-                    it[this.memberId] = userCreationEvent.id
-                    it[this.leagueId] = leagueId
-                }
-            }
-
-            val globalLeaderboard = caclulateGlobalLeaderboard()
-            val s3Client = S3Client { region = "eu-west-2" }
-            runBlocking {
-                // TODO: add logic to calculate match day
-                LeaderboardS3Service(s3Client, Environment.LeaderboardBucketName).writeLeaderboard(globalLeaderboard, 1)
-            }
+//            try {
+//                transaction {
+//                    LeagueTable.insert {
+//                        it[this.name] = "Global"
+//                        it[this.id] = "global"
+//                    }
+//                }
+//            } catch (e: PSQLException) {
+//                log.info("Global league exists: $e")
+//            }
+//
+//            transaction {
+//                LeagueMembershipTable.insert {
+//                    it[this.memberId] = userCreationEvent.id
+//                    it[this.leagueId] = leagueId
+//                }
+//            }
+//
+//            val globalLeaderboard = caclulateGlobalLeaderboard()
+//            val s3Client = S3Client { region = "eu-west-2" }
+//            runBlocking {
+//                // TODO: add logic to calculate match day
+//                LeaderboardS3Service(s3Client, Environment.LeaderboardBucketName).writeLeaderboard(globalLeaderboard, 1)
+//            }
         }
     }
 }
