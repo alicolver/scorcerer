fb92173a6c821cc8a00fb72f36b9ca854777066e
Simon Archer <simon.james.archer@gmail.com>
2024-05-25 00:45:57 +0100

    set flaguri to 100 characters

--- lambdas/src/main/kotlin/scorcerer/server/db/tables/TeamTable.kt
+++ lambdas/src/main/kotlin/scorcerer/server/db/tables/TeamTable.kt
@@ -5,5 +5,5 @@ import org.jetbrains.exposed.sql.Table
 object TeamTable : Table("team") {
     val id = integer("id").uniqueIndex().autoIncrement()
     val name = varchar("name", 30).uniqueIndex()
-    val flagUri = varchar("flag_uri", 60)
+    val flagUri = varchar("flag_uri", 100)
 }
