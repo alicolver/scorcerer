a5e0063b6e4a2f987975aeabcdfb3c3985f9b641
Luke Ely <elyluk@amazon.com>
2024-06-08 18:22:45 +0100

    Disable WRITE_DATES_AS_TIMESTAMPS

--- lambdas/templates/Jackson.kt
+++ lambdas/templates/Jackson.kt
@@ -1,10 +1,11 @@
 package org.openapitools.server
 
+import com.fasterxml.jackson.databind.SerializationFeature
 import com.fasterxml.jackson.module.kotlin.jacksonObjectMapper
 import com.fasterxml.jackson.module.kotlin.readValue
 import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule
 
-val mapper = jacksonObjectMapper().registerModule(JavaTimeModule())
+val mapper = jacksonObjectMapper().registerModule(JavaTimeModule()).disable(SerializationFeature.WRITE_DATES_AS_TIMESTAMPS)
 
 inline fun <reified A>A.toJson(): String = mapper.writeValueAsString(this)
 
