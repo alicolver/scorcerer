a0ff062bc7e4a072fda31e59f4ecf316cf83cf41
Luke Ely <elyluk@amazon.com>
2024-06-15 16:18:41 +0100

    Use fotmob instead of sofascore

--- lambdas/src/main/kotlin/scorcerer/server/schedule/ScoreUpdater.kt
+++ lambdas/src/main/kotlin/scorcerer/server/schedule/ScoreUpdater.kt
@@ -10,24 +10,25 @@ import org.openapitools.server.fromJson
 import scorcerer.server.log
 
 @JsonIgnoreProperties(ignoreUnknown = true)
-data class Score(
-    val current: Int,
-)
-
-data class SofaScoreResponse(
-    val event: SofaScoreEvent,
+data class Team(
+    val name: String,
+    val score: Int,
 )
 
 @JsonIgnoreProperties(ignoreUnknown = true)
-data class SofaScoreEvent(
-    val homeScore: Score,
-    val awayScore: Score,
+data class FotMobResponse(
+    val header: FotMobHeader,
+)
+
+@JsonIgnoreProperties(ignoreUnknown = true)
+data class FotMobHeader(
+    val teams: List<Team>,
 )
 
 class ScoreUpdater : RequestHandler<Unit, Unit> {
     val client = OkHttp()
-    private val endpoint = "https://www.sofascore.com/api/v1/event/"
-    private val eventId = "11873897"
+    private val endpoint = "https://www.fotmob.com/api/matchDetails?matchId="
+    private val eventId = "4043838"
 
     override fun handleRequest(input: Unit?, context: Context?) {
         log.info("Fetching the live score")
@@ -37,14 +38,16 @@ class ScoreUpdater : RequestHandler<Unit, Unit> {
 
         log.info("Response status - ${response.status}")
         log.info("Response body length - ${response.body.length}")
-        log.info(response.bodyString())
 
         if (!response.status.successful) {
+            log.info(response.bodyString())
             log.info("Response status not good, exiting")
             return
         }
 
-        val score = response.body.toString().fromJson<SofaScoreResponse>()
-        log.info("Fetch live score with home score (${score.event.homeScore.current}) and away score (${score.event.awayScore.current})")
+        val score = response.body.toString().fromJson<FotMobResponse>()
+        val homeScore = score.header.teams.first().score
+        val awayScore = score.header.teams.last().score
+        log.info("Home score ($homeScore) Away score ($awayScore)")
     }
 }
